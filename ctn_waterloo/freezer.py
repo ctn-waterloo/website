fd9fe2053a4288e21497c75f2a657c748f3e4635
Trevor Bekolay <tbekolay@gmail.com>
2013-07-08 10:38:46 -0400

    Added redirect URL generator

--- ctn_waterloo/freezer.py
+++ ctn_waterloo/freezer.py
@@ -1,9 +1,14 @@
 import os
 import mimetypes
 
-from . import app
+from . import app, redirects
 from flask.ext.frozen import Freezer, walk_directory
 
 # Seems to be available only on some systems...
 mimetypes.add_type('application/atom+xml', '.atom')
+
 freezer = Freezer(app)
+
+@freezer.register_generator
+def redirect_url_generator():
+    return redirects.keys()
