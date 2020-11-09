e275e2cd5777b84bf2974e3603dc745e67375cca
Andreas Stöckel <astoecke@uwaterloo.ca>
2020-11-09 04:17:09 -0500

    Add build_and_deploy.sh
    This script replaces .travis.yml. It is split into two stages.
    The first stage serves as an outer wrapper responsible for
    downloading the repository and executing the second-stage build
    script inside a sandbox (this is the .travis.yml part).
    
    In contrast to the old setup, the first stage is responsible for
    uploading the built website to the webserver. This is much simpler
    now since the build server will be inside the UW network.

---
+++ build_and_deploy.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+
+# This script is used to buid the website in the CI system. It replaces
+# the old .travis.yml.
+#
+# The basic idea is the following: The build system is split into two
+# phases. The "first stage" immutably resides on the build server. It
+# simply downloads the Git repository with the website into a temporary
+# directory and then proceeds to execute the "second stage" build script
+# inside a disposable sandbox.
+#
+# The second stage installs Python dependencies and performs the actual
+# build. Once that is done, the first stage resumes and uploads the
+# website to the webserver.
+#
+# This way the code in this repository is completely isolated from the
+# rest of the build server. This is less meant as a security measure
+# but to prevent the website code from accidentially breaking something.
+#
+# Note that changes to the "first stage" of the build script are not
+# automatically reflected on the build server. The build script (as well
+# as the webhook server) need to be manually updated by logging into
+# the server.
+#
+# Authors:
+# 	Andreas Stöckel, 2020
+
+# Exit if a command errors out
+set -e
+
+# Handy function for printing messages
+msg() {
+	echo -e "[build_and_deploy.sh:$STAGE]" $*
+}
+
+# cd into the directory this script resides in
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd "$DIR"
+
+# Fetch the stage from the command line arguments. Default to "first".
+STAGE=${1:-first}
+if [ $STAGE = "first" ]; then
+	# IMPORTANT: Changes to the first stage require manually updating
+	#            the build script on the build server!
+	msg "==> Entering first stage build"
+
+	# Create a random temporary directory
+	TMP=$(mktemp -d -t cnrg_website_build_XXXXXXXXXX)
+
+	# Make sure that the temporary directory is deleted when
+	# this script exists, no matter what the reason is.
+	trap 'rm -rf "$TMP"' EXIT
+
+	# Create the directories used by pip. The PIP_CACHE
+	# directory is kept at a separate, permanent location,
+	# so the second stage doesn't need to re-download and
+	# re-compile the pip dependencies all the time.
+	PIP_CACHE=/tmp/docker_pip_cache
+	if [ ! -d "$PIP_CACHE" ]; then
+		msg "Creating pip cache directory"
+		mkdir -p "$PIP_CACHE"
+		chmod 700 "$PIP_CACHE"
+	fi
+	mkdir "$TMP/local"
+	chmod 700 "$TMP/local"
+
+
+	# Clone the website into the directory
+	msg "Setting up website repository in $TMP"
+	#rsync -a . "$TMP/repo"
+	git clone https://github.com/ctn-waterloo/website "$TMP/repo" --depth 1
+
+	# Launch the second stage script inside a Docker container
+	msg "Launching Docker container with the second stage build script"
+	docker run \
+		-u "$(id -u):$(id -g)" \
+		-v "$TMP/repo":/repo:z \
+		-v "$TMP/local":/.local:z \
+		-v "$PIP_CACHE":/.cache/pip:z \
+		python:3.8-buster \
+		/repo/build_and_deploy.sh second
+
+	# Upload the compiled website
+	msg
+	msg "==> Re-entering first stage build"
+	msg "Uploading website to the webserver"
+	if [ -d "$TMP/ctn_waterloo/build" ]; then
+		rsync -ah --del "$TMP/ctn_waterloo/build/" cnrglab@compneuro.uwaterloo.ca:/home/cnrglab/public_html/
+	fi
+elif [ $STAGE = "second" ]; then
+	msg
+	msg "==> Entering second stage build"
+
+	# Install all requirements
+	msg "Installing Python dependencies"
+	pip install --user -r requirements.txt --progress-bar off --no-warn-script-location
+
+	# Build the website and store the results in the "build" directory
+	msg "Building the website"
+	./manage.py freeze
+fi
